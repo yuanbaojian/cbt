fefabdd991178acf28a570924b62d4c801a2c9e6
yuanbaojian <312ybj@gmail.com>
2019-11-28 09:17:44 +0800

    今日更新

--- src/main/java/com/ybj/cbt/Learn/ThinkingInJava/Ch18/GetBytes.java
+++ src/main/java/com/ybj/cbt/Learn/ThinkingInJava/Ch18/GetBytes.java
@@ -7,6 +7,7 @@ public class GetBytes {
     @Test
     public void getBytes(){
         String name="abc";
+
         byte[] bytes = name.getBytes();
         for (byte aByte : bytes) {
           System.out.println("aByte = " + aByte);
