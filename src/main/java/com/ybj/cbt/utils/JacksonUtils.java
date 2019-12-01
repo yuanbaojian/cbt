89d96e39e86f2e1cda7ea18ca3f74f5c805790ed
yuanbaojian <312ybj@gmail.com>
2019-12-01 23:32:57 +0800

    增加jsonnode的使用

--- src/main/java/com/ybj/cbt/utils/JacksonUtils.java
+++ src/main/java/com/ybj/cbt/utils/JacksonUtils.java
@@ -1,6 +1,7 @@
 package com.ybj.cbt.utils;
 
 import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 
@@ -8,6 +9,14 @@ public final class JacksonUtils {
 
     public static ObjectMapper objectMapper;
 
+
+    /**   对象转String
+     * @param object
+     * @return java.lang.String
+     * @author yuanbaojian
+     * @date 2019/12/1
+     * @time 22:56
+     */
     public static String objectToJsonString(Object object) {
         if (objectMapper == null) {
             objectMapper = new ObjectMapper();
@@ -20,13 +29,34 @@ public final class JacksonUtils {
         return null;
     }
 
+    /**  string转jsonNode 或 jsonNode数组
+     * @param jsonString
+     * @return com.fasterxml.jackson.databind.JsonNode
+     * @author yuanbaojian
+     * @date 2019/12/1
+     * @time 22:56
+     */
+    public static JsonNode stringToJsonNode(String jsonString){
+        if (objectMapper == null) {
+            objectMapper = new ObjectMapper();
+            objectMapper.configure(JsonParser.Feature.ALLOW_SINGLE_QUOTES, true);
+        }
+        try {
+            return objectMapper.readTree(jsonString);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
-    /**
-     *  使用 泛型
+
+    /**  jsongString转Object（对象或对象数组）
      * @param jsonStr
      * @param valueType
-     * @param <T>
-     * @return
+     * @return T
+     * @author yuanbaojian
+     * @date 2019/12/1
+     * @time 22:56
      */
     public static <T> T stringToObject(String jsonStr, Class<T> valueType) {
         if (objectMapper == null) {
