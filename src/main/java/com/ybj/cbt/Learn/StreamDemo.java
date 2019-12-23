47047245999327337bd7f51a6ba3d70ee1e89fbd
yuanbaojian <312ybj@gmail.com>
2019-12-23 23:13:04 +0800

    java 8 stream

---
+++ src/main/java/com/ybj/cbt/Learn/StreamDemo.java
@@ -0,0 +1,34 @@
+package com.ybj.cbt.Learn;
+
+import org.junit.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Stream;
+
+/**
+ * @Author StreamDemo
+ * @Description //TODO $
+ * @Date $ $
+ * @Param $
+ * @return $
+ **/
+public class StreamDemo {
+
+    @Test
+    public void test(){
+        List list=new LinkedList<String>();
+        list.add("hello");
+        list.add("world");
+        Stream stream=Stream.of(list);
+        System.out.println("stream = " + stream);
+        for (Object o : list) {
+          System.out.println("o = " + o);
+        }
+    }
+
+    @Test
+    public void test2(){
+
+    }
+}
---
+++ src/main/java/com/ybj/cbt/Learn/testReturn.java
@@ -0,0 +1,17 @@
+package com.ybj.cbt.Learn;
+
+/**
+ * @Author testReturn
+ * @Description //TODO $
+ * @Date $ $
+ * @Param $
+ * @return $
+ **/
+public final class testReturn {
+
+    public static int number=3;
+
+    public static testReturn getObject(){
+        return new testReturn();
+    }
+}
