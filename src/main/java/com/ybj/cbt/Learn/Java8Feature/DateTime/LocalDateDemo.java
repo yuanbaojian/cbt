33ea009903a18175a6723b1faf8895709ddb1f56
baojian.yuan <1793870688@qq.com>
2019-12-25 18:37:41 +0800

    补

---
+++ src/main/java/com/ybj/cbt/Learn/Java8Feature/DateTime/LocalDateDemo.java
@@ -0,0 +1,16 @@
+package com.ybj.cbt.Learn.Java8Feature.DateTime;
+
+import org.junit.Test;
+
+import java.time.LocalDate;
+
+public class LocalDateDemo {
+
+    @Test
+    public void test() {
+        LocalDate localDate = LocalDate.now();
+
+    }
+
+
+}
