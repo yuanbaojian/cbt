ff908c4d165f9d429c2968a1cf83f83385c2ea77
yuanbaojian <312ybj@gmail.com>
2020-01-01 22:18:15 +0800

    持有对象研究

---
+++ src/main/java/com/ybj/cbt/Learn/ThinkingInJava/Ch11/ArrayListDemo.java
@@ -0,0 +1,37 @@
+package com.ybj.cbt.Learn.ThinkingInJava.Ch11;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import static jdk.nashorn.internal.objects.Global.print;
+
+
+/**
+ * @Author ArrayListDemo
+ * @Description //TODO $
+ * @Date $ $
+ * @Param $
+ * @return $
+ **/
+public class ArrayListDemo {
+
+    @Test
+    public void testArrayList(){
+        ArrayList arrayList=new ArrayList<>(Arrays.asList(1,2,3));
+        System.out.println("arrayList = " + arrayList);
+        Collection collection;
+        List list;
+    }
+
+    public static void main(String[] args) {
+        ArrayList arrayList=new ArrayList<>(Arrays.asList(1,2,3));
+        System.out.println("arrayList = " + arrayList);
+        Collection collection;
+        List list;
+        print(arrayList);
+    }
+}
