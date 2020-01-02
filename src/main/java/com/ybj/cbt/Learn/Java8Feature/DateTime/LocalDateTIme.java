a4d7068e227863a5112385418a021f8c48247931
yuanbaojian <312ybj@gmail.com>
2020-01-02 23:53:49 +0800

    localDateTime

--- src/main/java/com/ybj/cbt/Learn/Java8Feature/DateTime/LocalDateTIme.java
+++ src/main/java/com/ybj/cbt/Learn/Java8Feature/DateTime/LocalDateTIme.java
@@ -48,13 +48,26 @@ public class LocalDateTIme {
         LocalDateTime localDateTime=LocalDateTime.now();
         System.out.println("localDateTime = " + localDateTime);
         //localDateTime = 2019-12-23T22:22:32.358
-//        LocalDateTime localDateTime2=LocalDateTime.parse("2019-12-23T22:22:32.358", DateTimeFormatter.of)
 
         LocalDateTime localDateTime1=LocalDateTime.of(2011,1,1,1,1,1);
         System.out.println("localDateTime1 = " + localDateTime1);
         String localDate=localDateTime.toLocalDate().format(DateTimeFormatter.ofPattern("yyyy/MM/dd"));
         System.out.println("localDate = " + localDate);
         //localDate = 2019/12/23
+    }
+
+    @Test
+    public void StringToLocalDate(){
+        String dateTimeString="2019/12/23 22:22:32";
+        LocalDateTime  localDateTime=LocalDateTime.parse(dateTimeString, DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss"));
+        System.out.println("localDateTime = " + localDateTime);
+    }
+
+    @Test
+    public void LocalDateToString(){
+        LocalDateTime localDateTime= LocalDateTime.now();
+        String locadateTimeString=localDateTime.format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm"));
+        System.out.println("locadateTimeString = " + locadateTimeString);
     }
 
 }
